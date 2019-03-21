ca3a1386a43077b3613132c3f43f623be22e7a82
Rishi Khan <rishi.rkhan@gmail.com>
2019-03-21 15:24:08 -0400

    Update MockSwifterServer.swift

--- MockSwifterServer.swift
+++ MockSwifterServer.swift
@@ -3,7 +3,6 @@
 //  ecobeeUITests
 //
 //  Created by Rishi Khan on 2019-01-03.
-//  Copyright © 2019 ecobee. All rights reserved.
 //
 
 import Foundation
@@ -15,82 +14,87 @@ enum HTTPMethod {
     case PUT
 }
 
+// MARK: - HTTPStubInfo
+
 struct HTTPStubInfo {
     let url: String
     let jsonFilename: String
     let method: HTTPMethod
 }
 
+//Place the correct responses here for normal api calls
 let initialStubs = [
     HTTPStubInfo(url: "/1/user", jsonFilename: "user", method: .GET),
     HTTPStubInfo(url: "/authorize", jsonFilename: "authorize", method: .POST),
     HTTPStubInfo(url: "/token", jsonFilename: "token", method: .POST),
+    HTTPStubInfo(url: "/1/thermostat", jsonFilename: "tstat", method: .GET),
+    HTTPStubInfo(url: "/1/thermostatSummary", jsonFilename: "tstatSum", method: .GET),
+    HTTPStubInfo(url: "/1/group", jsonFilename: "group", method: .GET),
+    HTTPStubInfo(url: "/ea/devices", jsonFilename: "eaDevices", method: .GET),
+    HTTPStubInfo(url: "/ea/devices/ls", jsonFilename: "eaDevicesLs", method: .GET),
     HTTPStubInfo(url: "/graphql", jsonFilename: "", method: .POST)
 ]
-struct graphQuery {
-    let queryName: String
+
+// MARK: - _Name
+
+struct GraphQuery {
+    let name: String
     let jsonResponseFile: String
 }
 
+//Place the responses here for the graphql calls. The query name should be anything unique in the
+// graph's query variable. In these examples i used the substring right after the word 'query'
 let graphDict = [
-    graphQuery(queryName: "RootModelHomeSettings",jsonResponseFile: "RootModelHomeSettings"),
-    graphQuery(queryName:"HomeDevices",jsonResponseFile:"HomeDevices")
+    GraphQuery(name: "RootModelHomeSettings", jsonResponseFile: "RootModelHomeSettings"),
+    GraphQuery(name: "Vacations", jsonResponseFile: "vacations"),
+    GraphQuery(name: "HomeDevices", jsonResponseFile: "HomeDevices")
 ]
 
-
 class MockServer {
-    
+
     var server = HttpServer()
-    
+
     func setUp() {
         try! server.start(port)
         setupInitialStubs()
     }
-    
+
     func tearDown() {
         server.stop()
     }
-    
+
     func setupInitialStubs() {
         for stub in initialStubs {
             setupStub(url: stub.url, filename: stub.jsonFilename, method: stub.method)
         }
     }
-    
-    public func setupStub(url: String, filename: String, method: HTTPMethod) {
-        var jfilename: String?
 
-            let response: ((HttpRequest) -> HttpResponse) = { request in
-                if !(request.body.isEmpty) && request.queryParams.isEmpty{
-                    let req_body = String(bytes: request.body, encoding: .utf8)
-                    for query in graphDict{
-                        if (req_body?.contains(query.queryName))!{
-                            jfilename = query.jsonResponseFile
-                            break
-                        }
-                    }
-                }
-                else {
-                    jfilename = filename
-                }
-                let testBundle = Bundle(for: type(of: self))
-                let filePath = testBundle.path(forResource: jfilename, ofType: "json")
-                let fileUrl = URL(fileURLWithPath: filePath!)
-                let data = try! Data(contentsOf: fileUrl, options: .uncached)
-                let json = self.dataToJSON(data: data)
-                return HttpResponse.ok(.json(json as AnyObject))
+    public func setupStub(url: String, filename: String, method: HTTPMethod) {
+        let response: ((HttpRequest) -> HttpResponse) = { request in
+            var jfilename: String?
+            if !(request.body.isEmpty) && request.queryParams.isEmpty {
+                let req_body = String(bytes: request.body, encoding: .utf8)
+                jfilename = graphDict.first(where: { req_body?.contains($0.name) ?? false })?.jsonResponseFile
+            } else {
+                jfilename = filename
             }
-            switch method {
-            case .GET :
-                server.GET[url] = response
-            case .POST:
-                server.POST[url] = response
-            case .PUT:
-                server.PUT[url] = response
-            }
+            let testBundle = Bundle(for: type(of: self))
+            let filePath = testBundle.path(forResource: jfilename, ofType: "json")
+            let fileUrl = URL(fileURLWithPath: filePath!)
+            let data = try! Data(contentsOf: fileUrl, options: .uncached)
+            let json = self.dataToJSON(data: data)
+            return HttpResponse.ok(.json(json as AnyObject))
         }
-    
-    
+        switch method {
+        case .GET :
+            server.GET[url] = response
+        case .POST:
+            server.POST[url] = response
+        case .PUT:
+            server.PUT[url] = response
+        }
+    }
+
     func dataToJSON(data: Data) -> Any? {
         do {
             return try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
@@ -103,7 +107,5 @@ class MockServer {
     // MARK: Private
 
     let port: UInt16 = 2300
-    
-}
-//
 
+}
